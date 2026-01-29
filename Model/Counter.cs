778eadcf5c83b88717d66b67ed64ec74a28fc5a1
Kyo-XZ <ms.noctis@gmail.com>
2026-01-29 20:15:51 +0100

    Ajoutez des fichiers projet.

---
+++ Model/Counter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brasserie.Model
+{
+    public class Counter
+    {
+        public Counter()
+        {
+            CounterValue = 0;
+            LimitiMax = 10;
+            PreAlarmThresold = 8;
+            UpdatePreAlarmStatus();
+        }
+
+        public int CounterValue { get; set; }
+        public int LimitiMax { get; set; }
+        public int PreAlarmThresold { get; set; }
+        public bool IsInPreAlarm { get; private set; }
+
+        public void Increment()
+        {
+            if (CounterValue + 1 <= LimitiMax)
+            {
+                CounterValue++;
+                UpdatePreAlarmStatus();
+            }
+        }
+
+        private void UpdatePreAlarmStatus()
+        {
+            IsInPreAlarm = CounterValue >= PreAlarmThresold;
+        }
+    }
+}
